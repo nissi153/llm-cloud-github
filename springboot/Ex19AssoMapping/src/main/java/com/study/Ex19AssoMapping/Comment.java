35da4be68b98361fe7bef652e5f4a07217f31ef4
nissi153 <nissisoft21@gmail.com>
2026-03-24 11:22:12 +0900

    Update Comment.java

--- springboot/Ex19AssoMapping/src/main/java/com/study/Ex19AssoMapping/Comment.java
+++ springboot/Ex19AssoMapping/src/main/java/com/study/Ex19AssoMapping/Comment.java
@@ -1,4 +1,26 @@
 package com.study.Ex19AssoMapping;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.ManyToOne;
+import lombok.*;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Comment {
+  private Long id;
+  private String content;
+
+//  private Long board_id; //게시글의 id - 외래키(FK)
+
+  // LAZY(느린, 지연) : board 객체를 필요할 때(get)때 가져온다.
+  //                 Board엔티티가 준비되고 나서 가져와야 됨.
+  // EAGER(열렬, 즉시) : comment엔티티 생성시 가져온다.
+  @ManyToOne(fetch = FetchType.LAZY)
+  private Board board; //Board엔티티의 객체를 매핑해 준다.
 }
+// 댓글을 조회하려면, 게시글의 id를 fk필드로 가진 레코드들을 검색했다.
+// Select * from comment where board_id = 3;
