4cef1475183e018cf769601ea1e2eeacb30a62d8
nissi153 <nissisoft21@gmail.com>
2026-04-09 15:05:17 +0900

    업데이트

---
+++ python_basic/ex01.py
@@ -0,0 +1,23 @@
+# 한줄 코멘트
+# 자동완성 기능 온/오프
+#  - 상태 표시줄 Cursor Tab
+
+# 여러줄 코멘트
+"""
+여러줄 코멘트
+"""
+
+# HTML/CSS/JS 포맷터(프리티어)
+# 파이썬 포맷터(블랙 포맷터)
+# 1. 플러그인에서 Black 검색
+# 2. 설정(CTRL + ,)에서
+# 3. Format on Save : 체크한다.
+# 4. Default Formatter -> Black Formatter
+# 단축키 : CTRL + S(CTRL+ALT+F)
+# 정렬안됨.
+a = 10
+b = 20
+print(a + b)
+
+# CTRL + I  AI채팅창 열고/닫기
+# CTRL + J  하단 출력창 열고/닫기
